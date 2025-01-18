40beca0214a312340626ac4203ca29a0db70211c
Jade Miller <mil19017@byui.edu>
2025-01-17 17:55:40 -0700

    success

--- lab01.py
+++ lab01.py
@@ -7,17 +7,20 @@ guess_message = 0
 random_int = random.randint(1, positive_int)
 guess_message = int(input(f"Guess a number between 1 and {positive_int}: "))
 tally_guess = 0
+list_guess = []
 
 while guess_message != random_int:
     tally_guess += 1
-    list_guess = []
+    list_guess.append(guess_message)
+    
     if guess_message > random_int:
         print("   Too high!")
         guess_message = int(input(f"Guess a number between 1 and {positive_int}: "))
+
     elif guess_message < random_int:
         print("   Too low!")
         guess_message = int(input(f"Guess a number between 1 and {positive_int}: "))
 
-list_guess.append(guess_message)
 
+list_guess.append(guess_message)
 print(f"You were able to find the number in {tally_guess + 1} guesses.\nThe numbers you guessed were: {list_guess}")
